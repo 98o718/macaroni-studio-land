d4ae3cb8553a4a2e8fade0855bed231281803fab
98o718 <kalyakin.maks@mail.ru>
2019-04-23 22:12:20 +0300

    Fixing safari video autoplay

--- src/components/SpaghettiVideo.jsx
+++ src/components/SpaghettiVideo.jsx
@@ -18,7 +18,7 @@ const Video = styled.video({
 
 export const SpaghettiVideo = () => {
   return (
-    <Video loop autoPlay muted playsinline>
+    <Video loop autoPlay muted playsInline>
       <source src={pasta} type="video/mp4" />
       Ваш браузер не поддерживает тег video{' '}
       <span role="img" aria-label="Грустный эмодзи">

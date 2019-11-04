644ac523b4a1cb6d84b20682a1206bf82fb3879d
98o718 <kalyakin.maks@mail.ru>
2019-11-04 21:07:09 +0300

    Fun with video

--- src/components/SpaghettiVideo.jsx
+++ src/components/SpaghettiVideo.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import styled from 'styled-components'
 
 import pasta from '../assets/pasta.mp4'
+import boy from '../assets/boy.mp4'
 
 const Video = styled.video`
   position: absolute;
@@ -19,7 +20,7 @@ const Video = styled.video`
 export const SpaghettiVideo = () => {
   return (
     <Video loop autoPlay muted playsInline>
-      <source src={pasta} type="video/mp4" />
+      <source src={boy} type="video/mp4" />
       Ваш браузер не поддерживает тег video{' '}
       <span role="img" aria-label="Грустный эмодзи">
         😔
